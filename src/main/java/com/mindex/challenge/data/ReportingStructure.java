415f3a7e73c85080c5b801714e0fbbc1c94ea18c
Mohammed Alam <ma3655@g.rit.edu>
2022-01-13 14:26:46 -0500

    I finished Task 1

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -5,20 +5,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+import com.mindex.challenge.controller.EmployeeController;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReportingStructure {
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
     
-    private Employee emEmployee;
+    private Employee myEmployee;
+    private List<Employee> directReportList;
     // total number of reports under a given employee ( see example in read me)
     private int numberOfReports;
+
+    public ReportingStructure(Employee employee){
+        this.myEmployee = employee;
+        this.directReportList = employee.getDirectReports();
+        this.numberOfReports = employee.getDirectReports().size();
+    }
 
     @GetMapping("/employee/{id}")
     public List<Employee> read(@PathVariable String id) {
         LOG.debug("Received employee create request for id [{}]", id);
 
-        return emEmployee.getDirectReports();
+        return directReportList;
     }
 }
